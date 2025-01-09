ebb141fa9a9eaf4cfe4c343f442ec1f3ec27e7af
fabihakhalid <fabihakhalid2005@gmail.com>
2025-01-09 06:08:50 -0800

    done by fabiha

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+	int addNumbers (int a, int b){
+	return a+b;
+}
+	int main () {
+	
+	int num1=10,num2=20;
+	cout<<"sum:"<<addNumbers<<"num1+num2"<<endl;
+	return 0;
+}
