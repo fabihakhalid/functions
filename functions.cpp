de7acd5c256ec2050a68f6039fa2d5cc79fbbbb5
fabihakhalid <fabihakhalid2005@gmail.com>
2025-01-10 06:53:02 -0800

    done by fabiha

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-	int addNumbers (int a, int b){
-	return a+b;
+	int subNumbers (int a, int b){
+	return a-b;
 }
 	int main () {
 	
 	int num1=10,num2=20;
-	cout<<"sum:"<<addNumbers<<"num1+num2"<<endl;
+	cout<<"sum:"<<subNumbers<<"num1-num2"<<endl;
 	return 0;
 }
---
+++ sub.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int subtractNumber(int a,int b) {
+
+return a-b;
+}
+
+int main(){
+	int num1= 10,num2= 20;
+	cout<<"difference:"<<subtractNumber(num1,num2 )<<endl;
+	return 0;
+}
