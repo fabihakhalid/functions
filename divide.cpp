09edb77fa2507234bf1d9c8f460addac14a10ae4
fabihakhalid <fabihakhalid2005@gmail.com>
2025-01-10 07:10:21 -0800

    done by fabiha

---
+++ divide.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+  int divideNumber (int a, int b){
+  
+  
+  return a%b;
+}
+  int main () {
+  	int num1=80, num2=5;
+  	cout<<"divide :"<<(num1,num2)<<endl;
+
+	
+	return 0;
+}
