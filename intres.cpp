e4019a5990fbc23c5ed1ef211ad712155eae40ef
fabihakhalid <fabihakhalid2005@gmail.com>
2025-01-15 06:40:42 -0800

    done by fabiha

---
+++ intres.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+double calculateInterest(double principal, double rate, double time) {
+    return (principal * rate * time) / 100;
+}
+
+int main() {
+    double principal = 1000, rate = 5, time = 2;
+    cout << "Simple Interest: " << calculateInterest(principal, rate, time) << endl; 
+    return 0;
+}
+
+
