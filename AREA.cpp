a44f75888a8d4442141519e9ad7045b9ed5932a1
Fabiha Khalid <fabihakhalid2005@gmail.com>
2025-02-02 06:35:35 -0800

    Update AREA.cpp

--- AREA.cpp
+++ AREA.cpp
@@ -5,7 +5,7 @@ int rectangleArea(int length, int width) {
     return length * width;
 }
 
-int main() {
+int main() { 
     int length = 10, width = 5;
     cout << "Area: " << rectangleArea(length, width) << endl; // Result: Area = 50
     return 0;
